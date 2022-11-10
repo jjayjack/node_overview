06ed5fabd24d1bcb06b75633dd2ad15c94547239
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-11-10 12:43:00 -0600

    npm validator testing

--- Node_Notes/app.js
+++ Node_Notes/app.js
@@ -1,3 +1,9 @@
+const validator = require('validator')
+
 const notes = require('./notes.js')
+
 const getNotes = notes()
 console.log(getNotes)
+
+// console.log(validator.isEmail('you@me.com'))
+console.log(validator.isURL('http://google.com'))
