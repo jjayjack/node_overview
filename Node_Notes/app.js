867b90867b6a4c5977ef6f18fe2b883614167672
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-11-10 15:44:44 -0600

    Initial utilizing npm yarg for commands

--- Node_Notes/app.js
+++ Node_Notes/app.js
@@ -1,10 +1,45 @@
-const notes = require('./notes.js')
 const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
 
-const command = process.argv[2]
+//customize yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
-	console.log('Adding note...')
-} else if (command === 'remove') {
-	console.log('Removing note...')
-}
+// Add
+yargs.command({
+	command: 'add',
+	describe: 'Add a new note',
+	handler: function () {
+		console.log('Adding a new note')
+	}
+})
+
+//Remove
+yargs.command({
+	command: 'remove',
+	describe: 'Remove a note',
+	handler: function () {
+		console.log('Removing a note')
+		console.log(chalk.black("hope it's the one you wanted"))
+	}
+})
+
+//list
+yargs.command({
+	command: 'list',
+	describe: 'Listing your notes',
+	handler: function () {
+		console.log('Listing...')
+	}
+})
+
+//read
+yargs.command({
+	command: 'read',
+	describe: 'Read a note',
+	handler: function () {
+		console.log('Reading...')
+	}
+})
+
+console.log(yargs.argv)
