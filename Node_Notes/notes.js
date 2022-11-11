8fe7d115e1bc16fc4c10b9e04d62765251373c87
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-11-11 14:02:45 -0600

    Updating addNote function to find method (finds first occurrence)

--- Node_Notes/notes.js
+++ Node_Notes/notes.js
@@ -7,8 +7,9 @@ const getNotes = () => {
 
 const addNote = (title, body) => {
 	const notes = loadNotes()
-	const duplicateNotes = notes.filter((note) => note.title === title)
-	if (duplicateNotes.length === 0) {
+	const duplicateNote = notes.find((note) => note.title === title)
+
+	if (!duplicateNote) {
 		notes.push({
 			title: title,
 			body: body
