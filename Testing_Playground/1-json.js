993e059d2dc8cc3216f791ff40769f496935796d
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-11-11 09:40:55 -0600

    JSON testing

--- Testing_Playground/1-json.js
+++ Testing_Playground/1-json.js
@@ -1,12 +1,23 @@
 const fs = require('fs')
-const book = {
-	title: 'A Court of Mist and Fury',
-	author: 'Sarah J. Maas'
-}
+// const book = {
+// 	title: 'A Court of Mist and Fury',
+// 	author: 'Sarah J. Maas'
+// }
 
-const bookJSON = JSON.stringify(book)
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
 
-console.log(bookJSON)
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
 
-const parsedData = JSON.parse(bookJSON)
-console.log(parsedData.author)
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const user = JSON.parse(dataJSON)
+user.name = 'John'
+userage = 38
+
+const userJSON = JSON.stringify(user)
+
+fs.writeFileSync('1-json.json', userJSON)
