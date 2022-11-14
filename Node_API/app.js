4134d8056ba64117b3a40efc3f4ddbbc798d5c01
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-11-14 11:34:51 -0600

    Formatting data from API call

--- Node_API/app.js
+++ Node_API/app.js
@@ -3,9 +3,15 @@ require('dotenv').config()
 const request = require('postman-request')
 const key = process.env.access_key
 
-const url = `http://api.weatherstack.com/current?access_key=${key}&query=37.8267,-122.4233`
+const url = `http://api.weatherstack.com/current?access_key=${key}&query=37.8267,-122.4233&units=f`
 
-request({ url: url }, (error, response) => {
-	const data = JSON.parse(response.body)
-	console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+	console.log(
+		response.body.current.weather_descriptions[0] +
+			': It is currently ' +
+			response.body.current.temperature +
+			' degrees out. It feels like ' +
+			response.body.current.feelslike +
+			' degrees out.'
+	)
 })
