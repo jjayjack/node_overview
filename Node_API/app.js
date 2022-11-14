3f47a0f18e8145ca9eae7786ea072c9caf72c2e5
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-11-14 09:59:05 -0600

    Starting npm initializer with postman-request

--- Node_API/app.js
+++ Node_API/app.js
@@ -1,11 +1,1 @@
-console.log('Start of program: Synchronous')
-
-setTimeout(() => {
-	console.log('2 second timer: Asynchronous')
-}, 2000)
-
-setTimeout(() => {
-	console.log('0 second timer: Asynchronous')
-}, 0)
-
-console.log('Stopping of program: Synchronous')
+const request = require('postman-request')
