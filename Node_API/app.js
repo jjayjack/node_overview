46859f75777b0d9dda4744cc467e3b02c239ddb3
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-11-14 10:26:29 -0600

    Setting up API call

--- Node_API/app.js
+++ Node_API/app.js
@@ -1,1 +1,6 @@
+require('dotenv').config()
+
 const request = require('postman-request')
+const key = process.env.access_key
+
+const url = `http://api.weatherstack.com/current?access_key=${key}&query=37.8267,-122.4233`
