650049f09914f934abcaadeaab0293031a49ae69
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-11-14 09:24:34 -0600

    Asynchronous Notes

--- Node_API/app.js
+++ Node_API/app.js
@@ -1,11 +1,11 @@
 console.log('Start of program: Synchronous')
 
 setTimeout(() => {
-	console.log('2 second timer')
+	console.log('2 second timer: Asynchronous')
 }, 2000)
 
 setTimeout(() => {
-	console.log('0 second timer')
+	console.log('0 second timer: Asynchronous')
 }, 0)
 
-console.log('Stopping of program')
+console.log('Stopping of program: Synchronous')
