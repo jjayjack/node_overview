3eac430ee466a1e89a11fd99740a8ce9dcb9b99f
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-11-15 16:23:18 -0600

    Utilizing 2nd API for location

--- Node_API/app.js
+++ Node_API/app.js
@@ -3,16 +3,16 @@ require('dotenv').config()
 const request = require('postman-request')
 const key = process.env.access_key
 
-const city = Chicago
-const state = IL
-const country = US
+const city = 'Chicago'
+const state = 'IL'
+const country = 'US'
 
 // const urlWeatherStack = `http://api.weatherstack.com/current?access_key=${key}&query=37.8267,-122.4233&units=f`
 const urlweather = `https://api.openweathermap.org/data/2.5/weather?lat=51.5085&lon=-0.1257&appid=${key}&units=imperial`
-const urlGEO = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=2&appid=${key}`
+const urlGEO = `http://api.openweathermap.org/geo/1.0/direct?q=Chicago,IL,US&limit=1&appid=${key}`
 
-request({ url: urlweather }, (error, response) => {
-	const dataJSON = JSON.parse(response.body)
+request({ url: urlweather, json: true }, (error, response) => {
+	const dataJSON = response.body
 
 	console.log(
 		'Current temperature is ' +
@@ -20,4 +20,16 @@ request({ url: urlweather }, (error, response) => {
 			' degrees, it currently feels like ' +
 			dataJSON.main.feels_like
 	)
+
+	if (error) {
+		console.log(error)
+	}
+})
+
+// const latlonRequest =
+request({ url: urlGEO, json: true }, (error, response) => {
+	const latitude = response.body[0].lat
+	const longitude = response.body[0].lon
+	console.log(latitude)
+	console.log(longitude)
 })
