ce94faf18493b42b22d630f593066c21249ef5a1
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-11-15 14:34:30 -0600

    API Call for Geolocation

--- Node_API/app.js
+++ Node_API/app.js
@@ -3,8 +3,13 @@ require('dotenv').config()
 const request = require('postman-request')
 const key = process.env.access_key
 
+const city = Chicago
+const state = IL
+const country = US
+
 // const urlWeatherStack = `http://api.weatherstack.com/current?access_key=${key}&query=37.8267,-122.4233&units=f`
 const urlweather = `https://api.openweathermap.org/data/2.5/weather?lat=51.5085&lon=-0.1257&appid=${key}&units=imperial`
+const urlGEO = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=2&appid=${key}`
 
 request({ url: urlweather }, (error, response) => {
 	const dataJSON = JSON.parse(response.body)
