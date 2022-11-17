4af522c7f291ca0e8bd40eae4df9bee60fda426e
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-11-17 15:29:48 -0600

    Setting up callback function for location

--- Node_API/app.js
+++ Node_API/app.js
@@ -8,9 +8,8 @@ const state = 'IL'
 const country = 'US'
 
 // const urlWeatherStack = `http://api.weatherstack.com/current?access_key=${key}&query=37.8267,-122.4233&units=f`
-const urlweather = `https://api.openweathermap.org/data/2.5/weather?lat=51.5085&lon=-0.1257&appid=${key}&units=imperial`
-const urlGEO = `http://api.openweathermap.org/geo/1.0/direct?q=Chicago,IL,US&limit=1&appid=${key}`
-
+// const urlweather = `https://api.openweathermap.org/data/2.5/weather?lat=51.5085&lon=-0.1257&appid=${key}&units=imperial`
+/*
 request({ url: urlweather, json: true }, (error, response) => {
 	if (error) {
 		console.log('Unable to connect to weather service')
@@ -20,11 +19,11 @@ request({ url: urlweather, json: true }, (error, response) => {
 		const dataJSON = response.body
 		console.log(
 			'Current temperature is ' +
-				dataJSON.main.temp +
-				' degrees, it currently feels like ' +
-				dataJSON.main.feels_like
-		)
-	}
+			dataJSON.main.temp +
+			' degrees, it currently feels like ' +
+			dataJSON.main.feels_like
+			)
+		}
 })
 
 // const latlonRequest =
@@ -39,4 +38,32 @@ request({ url: urlGEO, json: true }, (error, response) => {
 		console.log(latitude)
 		console.log(longitude)
 	}
+})
+*/
+
+const geocode = (address, callback) => {
+	const urlGEO =
+		'http://api.openweathermap.org/geo/1.0/direct?q=' +
+		address +
+		'&limit=1&appid=' +
+		key
+
+	request({ url: urlGEO, json: true }, (error, response) => {
+		if (error) {
+			callback('Unable to reach location services', undefined)
+		} else if (!response.body[0]) {
+			callback('Missing request information', undefined)
+		} else {
+			callback(undefined, {
+				latitude: response.body[0].lat,
+				longitude: response.body[0].lon,
+				location: response.body[0].name
+			})
+		}
+	})
+}
+
+geocode('LA,CA,US', (error, data) => {
+	console.log('Error:', error)
+	console.log('Data', data)
 })
