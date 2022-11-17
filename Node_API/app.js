4565837c0785fec9e7cfc5338a68d5d1eb3b4a7c
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-11-17 16:42:30 -0600

    Callback function for forecast

--- Node_API/app.js
+++ Node_API/app.js
@@ -1,31 +1,14 @@
 require('dotenv').config()
 
-const request = require('postman-request')
-const geocode = require('./utils/geocode.js')
-const key = process.env.access_key
-
-// const urlWeatherStack = `http://api.weatherstack.com/current?access_key=${key}&query=37.8267,-122.4233&units=f`
-// const urlweather = `https://api.openweathermap.org/data/2.5/weather?lat=51.5085&lon=-0.1257&appid=${key}&units=imperial`
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 /*
-request({ url: urlweather, json: true }, (error, response) => {
-	if (error) {
-		console.log('Unable to connect to weather service')
-	} else if (response.body.error) {
-		console.log('Unable to find location')
-	} else {
-		const dataJSON = response.body
-		console.log(
-			'Current temperature is ' +
-			dataJSON.main.temp +
-			' degrees, it currently feels like ' +
-			dataJSON.main.feels_like
-			)
-		}
-})
-
-*/
-
 geocode('Schererville,IN,US', (error, data) => {
+	console.log('Error:', error)
+	console.log('Data', data)
+})
+*/
+forecast(-75.7088, 44.1545, (error, data) => {
 	console.log('Error:', error)
 	console.log('Data', data)
 })
---
+++ Node_API/utils/forecast.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const request = require('postman-request')
+const key = process.env.access_key
+
+const forecast = (lat, lon, callback) => {
+	const urlweather =
+		'https://api.openweathermap.org/data/2.5/weather?lat=' +
+		lat +
+		'&lon=' +
+		lon +
+		'&appid=' +
+		key +
+		'&units=imperial'
+
+	request({ url: urlweather, json: true }, (error, response) => {
+		if (error) {
+			callback('Unable to reach weather services', undefined)
+		} else if (!response.body.coord) {
+			callback('Unable to find location', undefined)
+		} else {
+			callback(
+				undefined,
+				response.body.weather[0].description.toUpperCase() +
+					': Current temperature is ' +
+					response.body.main.temp +
+					' degrees, it currently feels like ' +
+					response.body.main.feels_like
+			)
+		}
+	})
+}
+
+module.exports = forecast
