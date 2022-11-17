a5e566befad516b176e001126d562742b3541933
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-11-17 17:02:14 -0600

    Combining callback output data

--- Node_API/app.js
+++ Node_API/app.js
@@ -6,8 +6,12 @@ geocode('Schererville,IN,US', (error, data) => {
 		return console.log(error)
 	}
 
-	forecast(data.latitude, data.longitude, (error, data) => {
-		console.log('Error:', error)
-		console.log('Data', data)
+	forecast(data.latitude, data.longitude, (error, forecastData) => {
+		if (error) {
+			return console.log(error)
+		}
+
+		console.log(data.city, data.state)
+		console.log(forecastData)
 	})
 })
