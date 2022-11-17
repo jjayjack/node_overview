aaed3d3b296d87ec52a013df40a8394daf84215d
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-11-17 16:57:36 -0600

    Clean up of console logs

--- Node_API/app.js
+++ Node_API/app.js
@@ -5,8 +5,6 @@ geocode('Schererville,IN,US', (error, data) => {
 	if (error) {
 		return console.log(error)
 	}
-	console.log('Error:', error)
-	console.log('Data', data)
 
 	forecast(data.latitude, data.longitude, (error, data) => {
 		console.log('Error:', error)
